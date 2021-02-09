48e66202365716ba38c9bfa0ce9ee5d1adc31909
Juliors1 <juliorojassolis@gmail.com>
2021-02-09 12:38:30 -0500

    finish assessment

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from .models import Item
 
-# Create your views here.
+
+class ItemList(ListView):
+    model = Item
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
+
+class ItemDelete(DeleteView):
+    model = Item
+    success_url = '/'
+
+class ItemUpdate(UpdateView):
+  model = Item
+  fields = ['description']
+  success_url = '/'
